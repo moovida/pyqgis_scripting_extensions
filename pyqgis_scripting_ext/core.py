beb48b289578b4a1ec2d6cd79a0b4653ea0304e4
Andrea Antonello <andrea.antonello@gmail.com>
2024-02-26 12:06:47 +0100

    first commit of base geometry and functionalities + testcases

---
+++ pyqgis_scripting_ext/core.py
@@ -0,0 +1,264 @@
+from qgis.core import *
+from qgis.gui import *
+from PyQt5.QtGui import QColor
+
+class HGeometry:
+    def __init__(self, geometry: any):
+        self.geometry = geometry
+        self.gqsGeometry = QgsGeometry(self.geometry.clone())
+
+    def __str__(self):
+        return self.asWkt()
+    
+    def asWkt(self) -> str:
+        return self.geometry.asWkt()
+    
+    def coordinates(self) -> list[tuple[float, float]]:
+        if isinstance(self.geometry, HPoint):
+            return [(self.x, self.y)]  
+        elif isinstance(self.geometry, HLineString):
+            return [(p.x, p.y) for p in self.geometry.points]
+        elif isinstance(self.geometry, HPolygon):
+            return [p.coordinates() for p in self.geometry.exterior_ring().points]
+        elif isinstance(self.geometry, QgsPoint):
+            return [(self.geometry.x(), self.geometry.y())]
+        elif isinstance(self.geometry, QgsMultiPoint):
+            return [(p.x(), p.y()) for p in self.geometry.vertices()]
+        elif isinstance(self.geometry, QgsLineString):
+            return [(p.x(), p.y()) for p in self.geometry.points()]
+        elif isinstance(self.geometry, QgsMultiLineString):
+            return [[(p.x(), p.y()) for p in line.points()] for line in self.geometry]
+        elif isinstance(self.geometry, QgsPolygon):
+            return [(p.x(), p.y()) for p in self.geometry.exteriorRing().points()]
+        elif isinstance(self.geometry, QgsMultiPolygon):
+            return [[(p.x(), p.y()) for p in ring.vertices()] for ring in self.geometry]
+        else:
+            raise ValueError(f"Unsupported geometry type: ${type(self.geometry)}")
+        
+    def area(self) -> float:
+        return self.geometry.area()
+    
+    def length(self) -> float:
+        return self.gqsGeometry.length()
+    
+    def distance(self, other) -> float:
+        return self.gqsGeometry.distance(QgsGeometry(other.geometry.clone()))
+    
+    def bbox(self) -> list[float]:
+        rect = self.geometry.boundingBox()
+        return [rect.xMinimum(), rect.yMinimum(), rect.xMaximum(), rect.yMaximum()]
+    
+    def child_count(self) -> int:
+        if isinstance(self.geometry, QgsPoint) or isinstance(self.geometry, QgsCurve) or isinstance(self.geometry, QgsLineString) or isinstance(self.geometry, QgsPolygon):
+            return 1
+        return self.geometry.childCount()
+    
+    def child(self, index: int):
+        if isinstance(self.geometry, QgsPoint) or isinstance(self.geometry, QgsCurve) or isinstance(self.geometry, QgsLineString) or isinstance(self.geometry, QgsPolygon):
+            return HGeometry.from_specialized(self.geometry)                
+        childGeom = self.geometry.childGeometry(index)
+        return HGeometry.from_specialized(childGeom)
+    
+    def intersects(self, other) -> bool:
+        return self.gqsGeometry.intersects(QgsGeometry(other.geometry.clone()))
+    
+    def contains(self, other) -> bool:
+        return self.gqsGeometry.contains(QgsGeometry(other.geometry.clone()))
+    
+    def touches(self, other) -> bool:
+        return self.gqsGeometry.touches(QgsGeometry(other.geometry.clone()))
+    
+    # def intersection(self, other):
+    #     return HGeometry.from_specialized(self.gqsGeometry.intersection(QgsGeometry(other.geometry.clone())))
+
+
+    @staticmethod
+    def fromWkt(wkt: str):
+        geom = QgsGeometry.fromWkt(wkt)
+        return HGeometry.from_specialized(geom)
+
+    @staticmethod
+    def from_specialized(geom: any):
+        if isinstance(geom, QgsGeometry):
+            geom = geom.constGet()
+
+        if not geom:
+            return None
+        if isinstance(geom, QgsPoint):
+            return HPoint(qgs_point_geom=geom)
+        elif isinstance(geom, QgsMultiPoint):
+            return HMultiPoint(qgs_multipoint_geom=geom)
+        elif isinstance(geom, QgsLineString):
+            return HLineString(qgs_linestring_geom=geom)
+        elif isinstance(geom, QgsMultiLineString):
+            return HMultiLineString(qgs_multilinestring_geom=geom)
+        elif isinstance(geom, QgsPolygon):
+            poly = HPolygon(qgs_polygon_geom=geom)
+            return poly
+        elif isinstance(geom, QgsMultiPolygon):
+            return HMultiPolygon(qgs_multipolygon_geom=geom)
+        elif isinstance(geom, QgsCurve):
+            return HLineString([HPoint(p.x(), p.y()) for p in geom.vertices()])
+        else:
+            raise ValueError(f"Unsupported geometry type: ${type(geom)}")
+        
+    
+
+class HPoint(HGeometry):
+    def __init__(self, x: float = None, y: float = None, qgs_point_geom: QgsPoint = None):
+        if qgs_point_geom:
+            self.x = qgs_point_geom.x()
+            self.y = qgs_point_geom.y()
+            self.geometry = qgs_point_geom
+        else:
+            self.x = x
+            self.y = y
+            self.geometry = QgsPoint(x, y)
+        super().__init__(self.geometry)
+    
+    @classmethod
+    def fromQgsPoint(cls, point: QgsPoint):
+        return cls(point.x(), point.y())
+    
+    
+
+class HMultiPoint(HGeometry):
+    def __init__(self, points: list[HPoint] = None, qgs_multipoint_geom: QgsMultiPoint = None):
+        if qgs_multipoint_geom:
+            self.geometry = qgs_multipoint_geom
+        else:
+            self.geometry = QgsMultiPoint([QgsPoint(p.x, p.y) for p in points])
+        super().__init__(self.geometry)
+
+    @classmethod
+    def fromCoords(cls, coordinates: list[tuple[float, float]]):
+        points = [HPoint(x, y) for x, y in coordinates]
+        return cls(points = points)
+    
+class HLineString(HGeometry):
+    def __init__(self, points: list[HPoint] = None, qgs_linestring_geom: QgsLineString = None):
+        if qgs_linestring_geom:
+            self.geometry = qgs_linestring_geom
+        else:
+            self.geometry = QgsLineString([QgsPoint(p.x, p.y) for p in points])
+        super().__init__(self.geometry)
+
+    @classmethod
+    def fromCoords(cls, coordinates: list[tuple[float, float]]):
+        points = [HPoint(x, y) for x, y in coordinates]
+        return cls(points)
+
+class HMultiLineString(HGeometry):
+    def __init__(self, linestrings: list[HLineString] = None, qgs_multilinestring_geom: QgsMultiLineString = None):
+        if qgs_multilinestring_geom:
+            self.geometry = qgs_multilinestring_geom
+        else:
+            self.linestrings = linestrings
+            self.geometry = QgsMultiLineString()
+            linesToAdd = [QgsLineString([QgsPoint(c[0], c[1]) for c in ls.coordinates()]) for ls in linestrings]
+            for line in linesToAdd:
+                self.geometry.addGeometry(line)
+        super().__init__(self.geometry)
+
+    @classmethod
+    def fromCoords(cls, coordinates: list[list[tuple[float, float]]]):
+        linestrings = [HLineString.fromCoords(coords) for coords in coordinates]
+        return cls(linestrings)
+
+class HPolygon(HGeometry):
+    def __init__(self, exterior_ring: HLineString = None, qgs_polygon_geom: QgsPolygon = None):
+        if qgs_polygon_geom:
+            self.geometry = qgs_polygon_geom
+        else:
+            coords = exterior_ring.coordinates()
+            if coords[0] != coords[-1]:
+                raise ValueError("The first and last point of the exterior ring must be equal")
+            self.geometry = QgsPolygon(QgsLineString([QgsPoint(x, y) for x, y in coords]))
+        super().__init__(self.geometry)
+
+    @classmethod
+    def fromCoords(cls, coords: list[tuple[float, float]]):
+        exterior_ring = HLineString.fromCoords(coords)
+        return cls(exterior_ring)
+
+    def add_interior_ring(self, ring: HLineString):
+        # check if ring has first and last point equal
+        coords = ring.coordinates()
+        if coords[0] != coords[-1]:
+            raise ValueError("The first and last point of an interior ring must be equal")
+        self.geometry.addInteriorRing(ring.geometry)
+
+    def exterior_ring(self) -> HLineString:
+        return HGeometry.from_specialized(self.geometry.exteriorRing())
+    
+    def interior_rings_count(self):
+        return self.geometry.numInteriorRings()
+    
+    def interior_ring(self, index: int):
+        return HGeometry.from_specialized(self.geometry.interiorRing(index))
+
+class HMultiPolygon(HGeometry):
+    def __init__(self, polygons: list[HPolygon] = None, qgs_multipolygon_geom: QgsMultiPolygon = None):
+        if qgs_multipolygon_geom:
+            self.geometry = qgs_multipolygon_geom
+        else:
+            qgsPolygons = [poly.geometry for poly in polygons]
+            self.geometry = QgsMultiPolygon()
+            for poly in qgsPolygons:
+                self.geometry.addGeometry(poly)
+        super().__init__(self.geometry)
+
+    @classmethod
+    def fromCoords(cls, coords: list[list[tuple[float, float]]]):
+        polygons = [HPolygon.fromCoords(c) for c in coords]
+        return cls(polygons)
+
+
+class HMapCanvas():
+    def __init__(self, iface: QgisInterface):
+        if iface:
+            self.iface = iface
+            self.canvas = iface.mapCanvas()
+        else:
+            self.canvas = QgsMapCanvas()
+
+    @staticmethod
+    def new():
+        return HMapCanvas(None)
+    
+    def set_extent(self, eswn: list[float]):
+        self.canvas.setExtent(QgsRectangle(*eswn))
+
+    def set_extent(self, extent: QgsRectangle):
+        self.canvas.setExtent(extent)
+
+    def add_geometry(self, geometry: HGeometry, color: str = 'red', width: float = 2.0):
+        if isinstance(geometry, HPoint):
+            r = QgsRubberBand(self.canvas, Qgis.GeometryType.Point)
+            r.addGeometry(QgsGeometry(geometry.geometry))
+        elif isinstance(geometry, HLineString):
+            r = QgsRubberBand(self.canvas, Qgis.GeometryType.Line)
+            r.setColor(QColor(color))
+            r.setWidth(width)
+            r.addGeometry(QgsGeometry(geometry.geometry))
+        elif isinstance(geometry, HPolygon):
+            r = QgsRubberBand(self.canvas, Qgis.GeometryType.Polygon)
+            r.addGeometry(QgsGeometry(geometry.geometry))
+            fillColor = QColor(color)
+            fillColor.setAlphaF(0.5)
+            r.setColor(fillColor)
+            strokeColor = QColor(color)
+            r.setStrokeColor(strokeColor)
+            r.setWidth(width)
+
+    def remove_all_geometries(self):
+        if self.iface:
+            for item in self.iface.mapCanvas().scene().items():
+                if isinstance(item, QgsRubberBand):
+                    self.iface.mapCanvas().scene().removeItem(item)
+        else:
+            raise ValueError("Cannot remove geometries from a non-iface map canvas")
+            
+
+    def show(self):
+        self.canvas.show()
---
+++ tests/test_ext.py
@@ -0,0 +1,265 @@
+import sys
+sys.path.append("./")
+import unittest
+from pyqgis_scripting_ext.core import *
+
+
+
+class TestPyQgisExt(unittest.TestCase):
+
+    # before the tests create test geoms
+    def setUp(self):
+        self.g1 = HPolygon.fromCoords([[0, 0], [0, 5], [5, 5], [5, 0], [0, 0]])
+        self.g2 = HPolygon.fromCoords([[5, 0], [5, 2], [7, 2], [7, 0], [5, 0]])
+        self.g3 = HPoint(4, 1)
+        self.g4 = HPoint(5, 4)
+        self.g5 = HLineString.fromCoords([[1, 0], [1, 6]])
+        self.g6 = HPolygon.fromCoords([[3, 3], [3, 6], [6, 6], [6, 3], [3, 3]])
+
+    def test_HPoint(self):
+        p = HPoint(1, 2)
+        self.assertIsInstance(p, HPoint)
+        self.assertIsInstance(p.geometry, QgsPoint)
+        self.assertEqual(p.x, 1)
+        self.assertEqual(p.y, 2)
+        self.assertEqual(p.asWkt(), "Point (1 2)")
+
+    def test_HLineString(self):
+        p1 = HPoint(1, 2)
+        p2 = HPoint(3, 4)
+        ls = HLineString([p1, p2])
+        self.assertIsInstance(ls, HLineString)
+        self.assertIsInstance(ls.geometry, QgsLineString)
+        self.assertEqual(ls.asWkt(), "LineString (1 2, 3 4)")
+    
+    def test_hlinestring_from_coordinates_list(self):
+        ls = HLineString.fromCoords([(1, 2), (3, 4)])
+        self.assertIsInstance(ls, HLineString)
+        self.assertIsInstance(ls.geometry, QgsLineString)
+        self.assertEqual(ls.asWkt(), "LineString (1 2, 3 4)")
+        
+        ls = HLineString.fromCoords([[1, 2], [3, 4]])
+        self.assertIsInstance(ls, HLineString)
+        self.assertIsInstance(ls.geometry, QgsLineString)
+        self.assertEqual(ls.asWkt(), "LineString (1 2, 3 4)")
+
+    def test_HPolygon(self):
+        # coordinates list one
+        coords1 = [(1, 2), (3, 4), (5, 6), (7, 8), (1, 2)]
+        ls1 = HLineString.fromCoords(coords1)
+
+        poly = HPolygon(ls1)
+        self.assertIsInstance(poly, HPolygon)
+        self.assertIsInstance(poly.geometry, QgsPolygon)
+        self.assertEqual(poly.asWkt(), "Polygon ((1 2, 3 4, 5 6, 7 8, 1 2))")
+
+    def test_hpolygon_from_coords_with_interior_ring(self):
+        coords1 = [(1, 2), (3, 4), (5, 6), (7, 8), (1, 2)]
+        poly = HPolygon.fromCoords(coords1)
+        holeCoords = [(9, 10), (11, 12), (13, 14), (9, 10)]
+        hole = HLineString.fromCoords(holeCoords)
+        poly.add_interior_ring(hole)
+
+        self.assertIsInstance(poly, HPolygon)
+        self.assertIsInstance(poly.geometry, QgsPolygon)
+        self.assertEqual(poly.asWkt(), "Polygon ((1 2, 3 4, 5 6, 7 8, 1 2),(9 10, 11 12, 13 14, 9 10))")
+
+    def test_HMultiPoint(self):
+        p1 = HPoint(1, 2)
+        p2 = HPoint(3, 4)
+        mp = HMultiPoint([p1, p2])
+        self.assertIsInstance(mp, HMultiPoint)
+        self.assertIsInstance(mp.geometry, QgsMultiPoint)
+        self.assertEqual(mp.asWkt(), "MultiPoint ((1 2),(3 4))")
+
+    def test_HMultiPoint_from_coords(self):
+        mp = HMultiPoint.fromCoords([(1, 2), (3, 4)])
+        self.assertIsInstance(mp, HMultiPoint)
+        self.assertIsInstance(mp.geometry, QgsMultiPoint)
+        self.assertEqual(mp.asWkt(), "MultiPoint ((1 2),(3 4))")
+
+    def test_HMultiLineString(self):
+        ls1 = HLineString.fromCoords([(1, 2), (3, 4)])
+        ls2 = HLineString.fromCoords([(5, 6), (7, 8)])
+        mls = HMultiLineString([ls1, ls2])
+        self.assertIsInstance(mls, HMultiLineString)
+        self.assertIsInstance(mls.geometry, QgsMultiLineString)
+        self.assertEqual(mls.asWkt(), "MultiLineString ((1 2, 3 4),(5 6, 7 8))")
+
+    def test_HMultiLineString_from_coords(self):
+        mls = HMultiLineString.fromCoords([[(1, 2), (3, 4)], [(5, 6), (7, 8)]])
+        self.assertIsInstance(mls, HMultiLineString)
+        self.assertIsInstance(mls.geometry, QgsMultiLineString)
+        self.assertEqual(mls.asWkt(), "MultiLineString ((1 2, 3 4),(5 6, 7 8))")
+
+    def test_HMultiPolygon(self):
+        coords1 = [(1, 2), (3, 4), (5, 6), (7, 8), (1, 2)]
+        poly1 = HPolygon.fromCoords(coords1)
+        coords2 = [(9, 10), (11, 12), (13, 14), (15, 16), (9, 10)]
+        poly2 = HPolygon.fromCoords(coords2)
+        mp = HMultiPolygon([poly1, poly2])
+        self.assertIsInstance(mp, HMultiPolygon)
+        self.assertIsInstance(mp.geometry, QgsMultiPolygon)
+        self.assertEqual(mp.asWkt(), "MultiPolygon (((1 2, 3 4, 5 6, 7 8, 1 2)),((9 10, 11 12, 13 14, 15 16, 9 10)))")
+
+    def test_HMultiPolygon_from_coords(self):
+        mp = HMultiPolygon.fromCoords([[(1, 2), (3, 4), (5, 6), (7, 8), (1, 2)], [(9, 10), (11, 12), (13, 14), (15, 16), (9, 10)]])
+        self.assertIsInstance(mp, HMultiPolygon)
+        self.assertIsInstance(mp.geometry, QgsMultiPolygon)
+        self.assertEqual(mp.asWkt(), "MultiPolygon (((1 2, 3 4, 5 6, 7 8, 1 2)),((9 10, 11 12, 13 14, 15 16, 9 10)))")
+
+    def test_child_geometries(self):
+        coords1 = [(1, 2), (3, 4), (5, 6), (7, 8), (1, 2)]
+        poly = HPolygon.fromCoords(coords1)
+        self.assertEqual(poly.child_count(), 1)
+
+        coords1 = [(1, 2), (3, 4), (5, 6), (7, 8), (1, 2)]
+        poly1 = HPolygon.fromCoords(coords1)
+        coords2 = [(9, 10), (11, 12), (13, 14), (15, 16), (9, 10)]
+        poly2 = HPolygon.fromCoords(coords2)
+        mp = HMultiPolygon([poly1, poly2])
+        self.assertEqual(mp.child_count(), 2)
+        child1 = mp.child(0)
+        self.assertIsInstance(child1, HPolygon)
+
+        ls = HLineString.fromCoords([(1, 2), (3, 4), (5, 6), (7, 8)])
+        self.assertEqual(ls.child_count(), 1)
+
+        ls1 = HLineString.fromCoords([(1, 2), (3, 4)])
+        ls2 = HLineString.fromCoords([(5, 6), (7, 8)])
+        mls = HMultiLineString([ls1, ls2])
+        self.assertEqual(mls.child_count(), 2)
+
+        p = HPoint(1, 2)
+        self.assertEqual(p.child_count(), 1)
+
+        p1 = HPoint(1, 2)
+        p2 = HPoint(3, 4)
+        mp = HMultiPoint([p1, p2])
+        self.assertEqual(mp.child_count(), 2)
+
+    def test_geometry_coordinates(self):
+        coords = [(1.0, 2.0), (3.0, 4.0), (5.0, 6.0), (7.0, 8.0), (1.0, 2.0)]
+        poly = HPolygon.fromCoords(coords)
+        self.assertEqual(poly.coordinates(), coords)
+
+        coords1 = [(1, 2), (3, 4), (5, 6), (7, 8), (1, 2)]
+        poly1 = HPolygon.fromCoords(coords1)
+        coords2 = [(9, 10), (11, 12), (13, 14), (15, 16), (9, 10)]
+        poly2 = HPolygon.fromCoords(coords2)
+        mp = HMultiPolygon([poly1, poly2])
+        self.assertEqual(mp.coordinates(), [coords1, coords2])
+
+        coords = [(1, 2), (3, 4)]
+        ls = HLineString.fromCoords(coords)
+        self.assertEqual(ls.coordinates(), coords)
+
+        coords1 = [(1, 2), (3, 4), (5, 6), (7, 8)]
+        coords2 = [(9, 10), (11, 12), (13, 14), (15, 16)]
+        mls = HMultiLineString.fromCoords([coords1, coords2])
+        self.assertEqual(mls.coordinates(), [coords1, coords2])
+
+        coords = [(1, 2), (3, 4)]
+        mp = HMultiPoint.fromCoords(coords)
+        self.assertEqual(mp.coordinates(), coords)
+
+        p = HPoint(1, 2)
+        self.assertEqual(p.coordinates(), [(1, 2)])
+
+
+    def test_geometry_from_wkt(self):
+        wkt = "POINT (1 2)"
+        p = HGeometry.fromWkt(wkt)
+        self.assertIsInstance(p, HPoint)
+
+        wkt = "MULTIPOINT ((1 2),(3 4))"
+        mp = HGeometry.fromWkt(wkt)
+        self.assertIsInstance(mp, HMultiPoint)
+
+        wkt = "LINESTRING (1 2, 3 4)"
+        ls = HGeometry.fromWkt(wkt)
+        self.assertIsInstance(ls, HLineString)
+
+        wkt = "MULTILINESTRING ((1 2, 3 4),(5 6, 7 8))"
+        mls = HGeometry.fromWkt(wkt)
+        self.assertIsInstance(mls, HMultiLineString)
+
+        wkt = "POLYGON ((1 2, 3 4, 5 6, 7 8, 1 2))"
+        poly = HGeometry.fromWkt(wkt)
+        self.assertIsInstance(poly, HPolygon)
+
+        wkt = "MULTIPOLYGON (((1 2, 3 4, 5 6, 7 8, 1 2)),((9 10, 11 12, 13 14, 15 16, 9 10)))"
+        mp = HGeometry.fromWkt(wkt)
+        self.assertIsInstance(mp, HMultiPolygon)
+
+    def test_area_and_length(self):
+        self.assertEqual(self.g1.area(), 25)
+        self.assertEqual(self.g1.length(), 20)
+
+        self.assertEqual(self.g2.area(), 4)
+        self.assertEqual(self.g2.length(), 8)
+
+        self.assertEqual(self.g3.area(), 0)
+        self.assertEqual(self.g3.length(), 0)
+
+        self.assertEqual(self.g4.area(), 0)
+        self.assertEqual(self.g4.length(), 0)
+
+        self.assertEqual(self.g5.area(), 0)
+        self.assertEqual(self.g5.length(), 6)
+        
+        self.assertEqual(self.g6.area(), 9)
+        self.assertEqual(self.g6.length(), 12)
+
+    def test_boundingbox(self):
+        self.assertEqual(self.g1.bbox(), [0, 0, 5, 5])
+        self.assertEqual(self.g2.bbox(), [5, 0, 7, 2])
+        self.assertEqual(self.g3.bbox(), [4, 1, 4, 1])
+        self.assertEqual(self.g4.bbox(), [5, 4, 5, 4])
+        self.assertEqual(self.g5.bbox(), [1, 0, 1, 6])
+        self.assertEqual(self.g6.bbox(), [3, 3, 6, 6])
+
+    def test_distance(self):
+        self.assertEqual(self.g4.distance(self.g5), 4)
+        self.assertEqual(self.g5.distance(self.g6), 2)
+
+    def test_predicate_methods(self):
+        self.assertTrue(self.g1.intersects(self.g2))
+        self.assertTrue(self.g1.intersects(self.g3))
+        self.assertTrue(self.g1.intersects(self.g4))
+        self.assertTrue(self.g1.intersects(self.g5))
+        self.assertTrue(self.g1.intersects(self.g6))
+
+        self.assertTrue(self.g1.touches(self.g2))
+        self.assertFalse(self.g1.touches(self.g3))
+        self.assertTrue(self.g1.touches(self.g4))
+        self.assertFalse(self.g1.touches(self.g5))
+        self.assertFalse(self.g1.touches(self.g6))
+
+        self.assertFalse(self.g1.contains(self.g2))
+        self.assertTrue(self.g1.contains(self.g3))
+        self.assertFalse(self.g1.contains(self.g4))
+        self.assertFalse(self.g1.contains(self.g5))
+        self.assertFalse(self.g1.contains(self.g6))
+
+    # def test_mapcanvas(self):
+    #     canvas = HMapCanvas.new()
+
+    #     # create a geometry per type
+    #     p = HPoint(1, 2)
+    #     ls = HLineString.fromCoords([(1, 2), (3, 4)])
+    #     poly = HPolygon.fromCoords([(1, 2), (3, 4), (5, 6), (7, 8), (1, 2)])
+        
+    #     # add the geometry to the map canvas with color and width
+    #     canvas.add_geometry(p, "red", 2)
+    #     canvas.add_geometry(ls, "blue", 2)
+    #     canvas.add_geometry(poly, "green", 2)
+
+    #     # set the extent of the map canvas
+    #     canvas.set_extent([0, 0, 10, 10])
+
+    #     # show
+    #     canvas.show()
+
+
+
